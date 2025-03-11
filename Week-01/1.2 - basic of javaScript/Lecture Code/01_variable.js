b57674ba3ffdf241dcc618abaf0f93b9961f5d74
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-03-11 15:02:52 +0530

    week 2

---
+++ Week-01/1.2 - basic of javaScript/Lecture Code/01_variable.js
@@ -0,0 +1,19 @@
+let firstName = "niyaf";
+console.log(firstName); // niyaf
+
+const age = 21;
+console.log(age); // 21
+
+var isStudent = true;
+console.log(isStudent); // true
+
+
+console.log("firstName"); // firstName
+console.log("Niyaf"); // Niyaf
+
+
+isStudent = "harkirath";
+console.log(isStudent); // harkirath
+
+isStudent = 10;
+console.log(isStudent); // 10
